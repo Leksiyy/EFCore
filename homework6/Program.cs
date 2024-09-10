1694aca5f285a69660809b4323d1196326a63b4d
Leksiyy <leksiy197618@gmail.com>
2024-09-10 17:29:46 +0300

    hotfix

--- homework6/Program.cs
+++ homework6/Program.cs
@@ -8,56 +8,68 @@ class Program
     {
         using (ApplicationContext db = new ApplicationContext())
         {
+            //1) Получить список студентов, зачисленных на определенный курс.
             var list1 = db.Enrollments
                 .Where(e => e.CourseId == 1)
                 .Select(e => e.Student)
                 .ToList();
             
+            // 2) Получить список курсов, на которых учит определенный преподаватель.
             var list2 = db.Instructors
                 .Where(e => e.Id == 2)
                 .Select(e => e.Course)
                 .ToList();
 
+            // 3) Получить список курсов, на которых учит определенный преподаватель, вместе с именами студентов, зачисленных на каждый курс.
             var list3 = db.Courses
                 .Where(e => e.Instructors.Any(e => e.Id == 1))
                 .Select(e => new
                 {
-                    Course = e,
-                    Student = e.Enrollments.Select(e => e.Student).ToList();
+                    Student = e.Enrollments.Select(e => e.Student.Name).ToList(),
+                    Course = e
                 }).ToList();
 
+            // 4) Получить список курсов, на которые зачислено более 5 студентов
             var list4 = db.Courses
                 .Where(e => e.Enrollments.Select(e => e.Student)
                     .Count() > 5).ToList();
             
+            // 5) Получить список студентов, старше 25 лет.
             var list5 = db.Students
                 .Where(e => (DateTime.Now.Year - e.Birthday.Year) > 25)
                 .ToList();
             
+            // 6) Получить средний возраст всех студентов.
             var answer6 = db.Students
                 .Average(e => e.Id);
 
+            // 7) Получить самого молодого студента.
             var answer7 = db.Students
                 .OrderBy(e => e.Birthday)
-                .FirstOrDefault()!;
+                .LastOrDefault()!;
 
+            // 8) Получить количество курсов, на которых учится студент с определенным Id.
             var list8 = db.Enrollments
                 .Where(e => e.StudentId == 1)
                 .Select(e => e.Course)
-                .ToList();
+                .Count();
 
+            // 9) Получить список имен всех студентов.
             var list9 = db.Students
                 .Select(e => e.Name)
                 .ToList();
 
+            // 10) Сгруппировать студентов по возрасту.
             var list10 = db.Students
                 .GroupBy(e => e.Birthday)
                 .ToList();
 
+            // 11) Получить список студентов, отсортированных по фамилии в алфавитном порядке.
             var list11 = db.Students
                 .OrderBy(e => e.Surname)
                 .ToList();
 
+            // 12) Получить список студентов вместе с информацией о зачислениях на курсы.
             var list12 = db.Students
                 .Join(db.Enrollments, 
                     student => student.Id,
@@ -69,18 +81,21 @@ class Program
                     })
                 .ToList();
 
+            // 13) Получить список студентов, не зачисленных на определенный курс.
             var list13 = db.Students
                 .Include(e => e.Enrollments)
                 .ThenInclude(e => e.Course)
                 .Where(e => e.Enrollments.Any(e => e.CourseId != 1))
                 .ToList();
 
+            // 14) Получить список студентов, зачисленных одновременно на два определенных курса.
             var list14 = db.Students
                 .Include(e => e.Enrollments)
                 .ThenInclude(e => e.Course)
-                .Where(e => e.Enrollments.Any(e => e.CourseId != 1 && e.CourseId != 2))
+                .Where(e => e.Enrollments.Any(e => e.CourseId == 1 && e.CourseId == 2))
                 .ToList();
 
+            // 15) Получить количество студентов на каждом курсе.
             var list15 = db.Enrollments
                 .GroupBy(e => e.Course.Name)
                 .Select(e => new
@@ -92,18 +107,3 @@ class Program
     }
 }
 
-//1) Получить список студентов, зачисленных на определенный курс.
-// 2) Получить список курсов, на которых учит определенный преподаватель.
-// 3) Получить список курсов, на которых учит определенный преподаватель, вместе с именами студентов, зачисленных на каждый курс.
-// 4) Получить список курсов, на которые зачислено более 5 студентов.
-// 5) Получить список студентов, старше 25 лет.
-// 6) Получить средний возраст всех студентов.
-// 7) Получить самого молодого студента.
-// 8) Получить количество курсов, на которых учится студент с определенным Id.
-// 9) Получить список имен всех студентов.
-// 10) Сгруппировать студентов по возрасту.
-// 11) Получить список студентов, отсортированных по фамилии в алфавитном порядке.
-// 12) Получить список студентов вместе с информацией о зачислениях на курсы.
-// 13) Получить список студентов, не зачисленных на определенный курс.
-// 14) Получить список студентов, зачисленных одновременно на два определенных курса.
-// 15) Получить количество студентов на каждом курсе.
